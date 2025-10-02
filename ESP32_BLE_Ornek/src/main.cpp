d291cbfc5538fbba4441a99d63990bea5b42dab6
aerasMAC <aeraslan80@gmail.com>
2025-10-02 21:18:28 +0300

    kod düzeni oluşturuldu.

--- ESP32_BLE_Ornek/src/main.cpp
+++ ESP32_BLE_Ornek/src/main.cpp
@@ -4,13 +4,7 @@
 #include <BLEUtils.h>
 #include <BLE2902.h>
 #include <Wire.h>
-//#include <Adafruit_Sensor.h>
-//#include <Adafruit_BME280.h>
 
-
-//Default Temperature is in Celsius
-//Comment the next line for Temperature in Fahrenheit
-#define rastgeleSayi1kullan
 
 //BLE Sunucu Adı
 #define bleSunucuAdi "ESP32_Sunucu"
