976a91c94d6bc87e8228f24ace76f5feb169f1c9
aerasMAC <aeraslan80@gmail.com>
2025-10-31 16:29:35 +0300

    değişiklikler eklendi.

--- ESP32_MachineLearning_Ornek/machineLearning/egitim.py
+++ ESP32_MachineLearning_Ornek/machineLearning/egitim.py
@@ -2,18 +2,18 @@ import tensorflow as tf
 from tensorflow import keras
 
 
-# Assume input shape is (3,) representing x, y, z values of accelerometer
+# Giriş şeklinin ivmeölçerin x, y, z değerlerini temsil eden (3,) olduğunu varsayalım
 model = keras.Sequential([
     keras.layers.InputLayer(input_shape=(3,)),
     keras.layers.Dense(16, activation='relu'),
     keras.layers.Dense(16, activation='relu'),
-    keras.layers.Dense(3, activation='softmax')  # 3 output classes for 3 gestures
+    keras.layers.Dense(3, activation='softmax')  # 3 eksen için 3 çıkış
 ])
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-# Train the model with your preprocessed sensor data
-model.fit(train_data, train_labels, epochs=10)
 
-# Save the model
-model.save("gesture_model.h5")
+model.fit(train_data, train_labels, epochs=10)# Modeli önceden işlenmiş sensör verilerile eğit
+
+
+model.save("gesture_model.h5")# modeli kaydet
